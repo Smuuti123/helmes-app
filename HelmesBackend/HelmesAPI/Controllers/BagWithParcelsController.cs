7dfb7a2541a43a967b77d497696175d6cacc4393
Smuutihh <92427508+Smuutihh@users.noreply.github.com>
2024-05-15 14:41:19 +0300

    Testing out BagWithParcel logic

--- HelmesBackend/HelmesAPI/Controllers/BagWithParcelsController.cs
+++ HelmesBackend/HelmesAPI/Controllers/BagWithParcelsController.cs
@@ -1,6 +1,8 @@
 using HelmesAPI.Data;
 using Microsoft.AspNetCore.Mvc;
 using HelmesAPI.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelmesAPI.Controllers;
 
@@ -13,5 +15,46 @@ public class BagWithParcelsController : ControllerBase
     public BagWithParcelsController(AppDbContext context)
     {
         _context = context;
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateBagWithParcels(BagWithParcels bag)
+    {
+        if(!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        if(await _context.BagWithParcels.AnyAsync(b => b.BagNumber == bag.BagNumber))
+        {
+            return BadRequest("Bag number must be unique");
+        }
+
+        if(bag.ListOfParcels == null || !bag.ListOfParcels.Any())
+        {
+            return BadRequest("List of parcels cannot be empyt");
+        }
+
+        _context.BagWithParcels.Add(bag);
+
+        try
+        {
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetBagWithParcels), new { id = bag.Id }, bag);
+        }
+        catch (DbUpdateException ex)
+        {
+            return StatusCode(500, "A database error occurred: " + ex.Message);
+        }
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<BagWithParcels>> GetBagWithParcels(int id)
+    {
+        var bagOfParcels = await _context.Parcels.FindAsync(id);
+
+        if(bagOfParcels == null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(bagOfParcels);
     }
 }
--- HelmesBackend/HelmesAPI/Models/Shipment.cs
+++ HelmesBackend/HelmesAPI/Models/Shipment.cs
@@ -3,10 +3,13 @@ namespace HelmesAPI.Models
     public class Shipment
     {
         public int Id { get; set;}
+        public bool IsFinalized { get; set;}
         public string? ShipmentNumber { get; set; } //Format “XXX-XXXXXX”, where X – letter or digit. Must be unique within entire database
         public Airport KnownAirport { get; set; }
         public string? FlightDate { get; set; } //Format “LLNNNN”, where L – letter, N – digit
-        public List<Parcel> ListOfParcels{ get; set; } = new List<Parcel>(); //List cannot be empty by the moment of dinalizing shipment
+        public List<BagWithParcels> BagsWithParcels { get; set; } = new List<BagWithParcels>();
+        public List<BagWithLetters> BagsWithLetters { get; set; } = new List<BagWithLetters>();
+        
     }
 
     public enum Airport
