dcb04f87094bf0b888f41d04515a3485cb52a7c8
Smuutihh <92427508+Smuutihh@users.noreply.github.com>
2024-05-20 02:27:19 +0300

    downlaoded more packages, made first unit test

---
+++ HelmesApp.Tests/ShipmentsControllerTests.cs
@@ -0,0 +1,48 @@
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelmesAPI.Controllers;
+using HelmesAPI.Data;
+using HelmesAPI.Models;
+using HelmesAPI.Protocol;
+
+namespace HelmesApp.Tests.Controllers
+{
+    public class ShipmentsControllerTests
+    {
+        private readonly ShipmentsController _controller;
+        private readonly AppDbContext _context;
+
+        public ShipmentsControllerTests()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            _context = new AppDbContext(options);
+            _controller = new ShipmentsController(_context);
+        }
+
+        [Fact]
+        public async Task PostShipment_InvalidShipmentNumber_ReturnsBadRequest()
+        {
+            var invalidShipmentRequest = new CreateShipmentRequest
+            {
+                ShipmentNumber = "invalid",
+                KnownAirport = Airport.TLL,
+                FlightNumber = "LL1234",
+                FlightDate = DateTime.Now.AddDays(1),
+            };
+
+            var result = await _controller.CreateShipment(invalidShipmentRequest);
+
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Shipment number must be in format 'XXX-XXXXXX', where X is a letter or digit.", badRequestResult.Value);
+        }
+    }
+}
